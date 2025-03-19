bbc7d46e23b3fd815b2b5b3d2ac74a3acf9d6815
Mikhael Setia Budi <161400240+MikhaelSetiaBudi@users.noreply.github.com>
2025-03-19 19:17:33 +0700

    Update Appointment.java

--- clinic-oop-Mikhael/src/Unguided/entities/Appointment.java
+++ clinic-oop-Mikhael/src/Unguided/entities/Appointment.java
@@ -9,24 +9,31 @@ package Unguided.entities;
  * @author ACER
  */
 public class Appointment {
+    // Atribut untuk menyimpan informasi dokter yang terlibat dalam janji temu
     private Doctor doctor;
+    // Atribut untuk menyimpan informasi pasien yang terlibat dalam janji temu
     private Patient patient;
+    // Atribut untuk menyimpan tanggal janji temu dalam format string
     private String date;
-
+    
+    // Konstruktor untuk membuat objek Appointment dengan dokter, pasien, dan tanggal tertentu.
     public Appointment(Doctor doctor, Patient patient, String date) {
         this.doctor = doctor;
         this.patient = patient;
         this.date = date;
     }
-
+    
+    // Mengembalikan dokter yang terlibat dalam janji temu.
     public Doctor getDoctor() {
         return doctor;
     }
-
+    
+    // Mengembalikan pasien yang terlibat dalam janji temu.
     public Patient getPatient() {
         return patient;
     }
-
+    
+    // Mengembalikan tanggal janji temu.
     public String getDate() {
         return date;
     }
