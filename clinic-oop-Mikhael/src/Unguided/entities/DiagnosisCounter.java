474d00dd3c79956422edc18212792102a9a0f40e
Mikhael Setia Budi <161400240+MikhaelSetiaBudi@users.noreply.github.com>
2025-03-19 19:31:36 +0700

    Update DiagnosisCounter.java

--- clinic-oop-Mikhael/src/Unguided/entities/DiagnosisCounter.java
+++ clinic-oop-Mikhael/src/Unguided/entities/DiagnosisCounter.java
@@ -12,13 +12,18 @@ import java.util.HashMap;
  * @author ACER
  */
 public class DiagnosisCounter {
+    // Map untuk menyimpan jumlah kemunculan setiap diagnosis.
     private static Map<String, Integer> diagnosisCount = new HashMap<>();
 
+    // Metode untuk menambahkan jumlah kemunculan suatu diagnosis.
     public static void incrementDiagnosis(String diagnosis) {
+        // Membalik string diagnosis sebelum menyimpannya dalam Map.
         diagnosis = new StringBuilder(diagnosis).reverse().toString(); 
+        // Menambahkan atau memperbarui jumlah diagnosis dalam Map
         diagnosisCount.put(diagnosis, diagnosisCount.getOrDefault(diagnosis, 0) + 1);
     }
-
+    
+    // Metode untuk mendapatkan jumlah kemunculan suatu diagnosis.
     public static int getDiagnosisCount(String diagnosis) {
         return diagnosisCount.getOrDefault(diagnosis, 0) + 1;
     }
