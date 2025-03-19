4f76ab775a8ae28a5089509f0b74e7682ae07c00
Mikhael Setia Budi <161400240+MikhaelSetiaBudi@users.noreply.github.com>
2025-03-19 19:35:04 +0700

    Update Doctor.java

--- clinic-oop-Mikhael/src/Unguided/entities/Doctor.java
+++ clinic-oop-Mikhael/src/Unguided/entities/Doctor.java
@@ -13,28 +13,36 @@ import java.util.List;
  * @author ACER
  */
 public class Doctor {
+    // Atribut untuk menyimpan nama dokter
     private String name;
+    // Atribut untuk menyimpan spesialisasi dokter
     private String specialization;
+    // Daftar janji temu yang dimiliki oleh dokter
     private List<Appointment> appointments;
 
+    // Konstruktor untuk membuat objek Doctor dengan nama dan spesialisasi tertentu.
     public Doctor(String name, String specialization) {
         this.name = name;
         this.specialization = specialization;
-        this.appointments = new ArrayList<>();
+        this.appointments = new ArrayList<>(); // Inisialisasi daftar janji temu
     }
 
+    // Mengembalikan nama dokter
     public String getName() {
         return name;
     }
 
+    // Mengembalikan spesialisasi dokter.
     public String getSpecialization() {
         return specialization;
     }
 
+    // Menambahkan janji temu ke daftar janji temu dokter.
     public void addAppointment(Appointment appointment) {
         appointments.add(appointment);
     }
-
+    
+    // Mengembalikan daftar janji temu dokter.
     public List<Appointment> getAppointments() {
         return appointments;
     }
